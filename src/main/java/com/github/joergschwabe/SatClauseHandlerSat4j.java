cbe9a3d2933310cdac0bff48114cf332a3c6b6ef
joergschwabe <joerg.schwabe@uni-ulm.de>
2019-10-09 12:38:46 +0200

    refactor

--- src/main/java/com/github/joergschwabe/SatClauseHandlerSat4j.java
+++ src/main/java/com/github/joergschwabe/SatClauseHandlerSat4j.java
@@ -2,7 +2,6 @@ package com.github.joergschwabe;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.liveontologies.puli.Inference;
