2a27c34959a4d7007f104070f19042ec42bd6e86
joergschwabe <joerg.schwabe@uni-ulm.de>
2019-10-28 00:02:16 +0100

    refactor

--- src/main/java/com/github/joergschwabe/CycleComputator.java
+++ src/main/java/com/github/joergschwabe/CycleComputator.java
@@ -31,49 +31,57 @@ public class CycleComputator {
 	/**
 	 * the current positions of iterators over inferences for conclusions
 	 */
-	private final Deque<Iterator<? extends Inference<? extends Integer>>> inferenceStack_ = new LinkedList<Iterator<? extends Inference<? extends Integer>>>();
+	private final Deque<Iterator<? extends Inference<? extends Integer>>> inferenceStack = new LinkedList<Iterator<? extends Inference<? extends Integer>>>();
+
+	/**
+	 * the current positions of iterators over conclusions
+	 */
+	private Deque<Iterator<Integer>> conclusionStack = new LinkedList<Iterator<Integer>>();
 
 	/**
 	 * contains all visited conclusions
 	 */
-	private final Deque<Integer> visited_ = new LinkedList<Integer>();
+	private final Deque<Integer> visited = new LinkedList<Integer>();
 
 	/**
 	 * the inferences of considered path
 	 */
-	private final List<Inference<? extends Integer>> inferencePath_ = new LinkedList<Inference<? extends Integer>>();
+	private final List<Inference<? extends Integer>> inferencePath = new LinkedList<Inference<? extends Integer>>();
 
-	private final List<Integer> conclusionPath_ = new LinkedList<Integer>();
+	/**
+	 * the objects of considered path
+	 */
+	private final List<Integer> conclusionPath = new LinkedList<Integer>();
 
-	private Set<Integer> consideredSCC = new HashSet<Integer>();
+	/**
+	 * the current considered stronly connected component
+	 */
+	private Set<Integer> scc = new HashSet<Integer>();
 
-	private int queryId;
-
-	private Deque<Iterator<Integer>> conclusionStack_ = new LinkedList<Iterator<Integer>>();
-
+	/**
+	 * contains all true inferences in model
+	 */
 	private Set<Inference<? extends Integer>> inferenceSet;
 
+	/**
+	 * contains all stronly connected components
+	 */
+	private final List<List<Integer>> sccs;
+	
 	public CycleComputator(final Proof<Inference<? extends Integer>> proof, int queryId) {
 		this.proof = proof;
-		this.queryId = queryId;
+		sccs = StronglyConnectedComponentsComputation.computeComponents(proof, queryId).getComponents();
 	}
 
 	public Set<Inference<? extends Integer>> getCycle(Set<Integer> conclusionSet, Set<Inference<? extends Integer>> inferenceSet) throws IOException, ParserException, ContradictionException {
 		this.inferenceSet = inferenceSet;
-		StronglyConnectedComponents<Integer> sccc = StronglyConnectedComponentsComputation.computeComponents(proof, queryId);
-		for(List<Integer> consideredSCC : sccc.getComponents()) {
-			consideredSCC.retainAll(conclusionSet);
-			if(consideredSCC.size() > 1) {
-				for(Integer concl : consideredSCC) {
-					conclusionStack_.clear();
-					inferenceStack_.clear();
-					inferencePath_.clear();
-					visited_.clear();
-					conclusionPath_.clear();
-					this.consideredSCC = new HashSet<Integer>(consideredSCC);
-					inferenceStack_.push(getInferences(concl).iterator());
-					visited_.add(concl);
-					conclusionPath_.add(concl);
+		for(List<Integer> scc : sccs) {
+			this.scc = new HashSet<Integer>(scc);
+			this.scc.retainAll(conclusionSet);
+			if(this.scc.size() > 1) {
+				for(Integer concl : this.scc) {
+					clear();
+					addObject(concl);
 					Set<Inference<? extends Integer>> cycle = findCycle();
 					if(cycle != null) {
 						return cycle;
@@ -84,35 +92,33 @@ public class CycleComputator {
 		return null;
 	}
 
-	private Deque<Inference<? extends Integer>> getInferences(Integer concl) {
-		Deque<Inference<? extends Integer>> infDeq = new LinkedList<>();
-		for(Inference<? extends Integer> inf : proof.getInferences(concl)){
-			if(inferenceSet.contains(inf)) {
-				infDeq.push(inf);
-			}
-		}
-		return infDeq;
+	private void clear() {
+		conclusionStack.clear();
+		inferenceStack.clear();
+		inferencePath.clear();
+		visited.clear();
+		conclusionPath.clear();
 	}
 
 	private Set<Inference<? extends Integer>> findCycle()
 			throws IOException, ParserException, ContradictionException {
 		for(;;) {
-			Iterator<? extends Inference<? extends Integer>> infIter = inferenceStack_.peek();
+			Iterator<? extends Inference<? extends Integer>> infIter = inferenceStack.peek();
 			if(infIter == null) {
 				return null;
 			}
 
 			if(infIter.hasNext()) {
 				Inference<? extends Integer> nextInf = infIter.next();
-				conclusionStack_.push(getPremises(nextInf).iterator());
-				inferencePath_.add(nextInf);
+				conclusionStack.push(getPremises(nextInf).iterator());
+				inferencePath.add(nextInf);
 			} else {
-				visited_.pop();
-				conclusionPath_.remove(conclusionPath_.size()-1);
-				inferenceStack_.pop();
+				visited.pop();
+				inferenceStack.pop();
+				conclusionPath.remove(conclusionPath.size()-1);
 			}
 
-			Iterator<? extends Integer> conclIter = conclusionStack_.peek();
+			Iterator<? extends Integer> conclIter = conclusionStack.peek();
 			if(conclIter == null) {
 				return null;
 			}
@@ -120,31 +126,36 @@ public class CycleComputator {
 			if(conclIter.hasNext()) {
 				Integer premise = conclIter.next();
 				
-				if(visited_.contains(premise)) {
-					List<Inference<? extends Integer>> list = inferencePath_.subList(conclusionPath_.indexOf(premise), inferencePath_.size());
-					return new HashSet<Inference<? extends Integer>>(list);
+				if(visited.contains(premise)) {
+					List<Inference<? extends Integer>> cycle = inferencePath.subList(conclusionPath.indexOf(premise), inferencePath.size());
+					return new HashSet<Inference<? extends Integer>>(cycle);
 				}
 
-				visited_.push(premise);
-				conclusionPath_.add(premise);
-				inferenceStack_.push(getInferences(premise).iterator());
-
+				addObject(premise);
 				continue;
 			}
-			conclusionStack_.pop();
-			inferencePath_.remove(inferencePath_.size()-1);
+			conclusionStack.pop();
+			inferencePath.remove(inferencePath.size()-1);
 		}
 
 	}
 
+	private void addObject(Integer object) {
+		visited.push(object);
+		conclusionPath.add(object);
+		inferenceStack.push(getInferences(object).iterator());
+	}
+
 	private Set<Integer> getPremises(Inference<? extends Integer> nextInf) {
-		Set<Integer> premises = new HashSet<Integer>();
-		for (Integer premise : nextInf.getPremises()) {
-			if (consideredSCC.contains(premise)) {
-				premises.add(premise);
-			}
-		}
+		Set<Integer> premises = new HashSet<Integer>(nextInf.getPremises());
+		premises.retainAll(scc);
 		return premises;
+	}
+	
+	private Deque<Inference<? extends Integer>> getInferences(Integer concl) {
+		Deque<Inference<? extends Integer>> infDeq = new LinkedList<Inference<? extends Integer>>(this.proof.getInferences(concl));
+		infDeq.retainAll(inferenceSet);
+		return infDeq;
 	}
 
 }
